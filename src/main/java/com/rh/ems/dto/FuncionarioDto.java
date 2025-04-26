81c4d502874b05a9536a829adb82137aeb84c13e
João Pedro <jpedrog475@gmail.com>
2025-04-26 19:41:12 -0300

    FuncionarioDTO

---
+++ src/main/java/com/rh/ems/dto/FuncionarioDto.java
@@ -0,0 +1,17 @@
+package com.rh.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioDto {
+    private Long id;
+    private String primeiroNome;
+    private String ultimonome;
+    private String email;
+}
