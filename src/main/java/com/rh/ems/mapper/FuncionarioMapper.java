62acaecc463c22dfd7c3325e43eb2bd7961d215e
João Pedro <jpedrog475@gmail.com>
2025-04-26 19:41:28 -0300

    FuncionarioMapper

---
+++ src/main/java/com/rh/ems/mapper/FuncionarioMapper.java
@@ -0,0 +1,23 @@
+package com.rh.ems.mapper;
+
+import com.rh.ems.dto.FuncionarioDto;
+import com.rh.ems.entity.Funcionario;
+
+public class FuncionarioMapper {
+    public static FuncionarioDto mapToFuncionarioDto(Funcionario funcionario){
+        return new FuncionarioDto(
+                funcionario.getId(),
+                funcionario.getPrimeiroNome(),
+                funcionario.getUltimoNome(),
+                funcionario.getEmail()
+        );
+    }
+    public static Funcionario mapToFuncionario(FuncionarioDto funcionarioDto){
+        return new Funcionario(
+                funcionarioDto.getId(),
+                funcionarioDto.getPrimeiroNome(),
+                funcionarioDto.getUltimonome(),
+                funcionarioDto.getEmail()
+        );
+    }
+}
