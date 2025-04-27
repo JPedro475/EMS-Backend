6b6ea89e04dcb1447401d65a7dc7c4f437d1b7d2
João Pedro <jpedrog475@gmail.com>
2025-04-26 21:21:07 -0300

    Funcionário Service

---
+++ src/main/java/com/rh/ems/service/FuncionarioService.java
@@ -0,0 +1,7 @@
+package com.rh.ems.service;
+
+import com.rh.ems.dto.FuncionarioDto;
+
+public interface FuncionarioService {
+    FuncionarioDto createFuncionario(FuncionarioDto funcionarioDto);
+}
