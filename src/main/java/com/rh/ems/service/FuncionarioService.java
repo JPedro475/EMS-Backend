800fdb2391c346a62f3f9a53369fd3f1199ab932
João Pedro <jpedrog475@gmail.com>
2025-04-26 22:01:51 -0300

    Criação da Lista de Funcionários

--- src/main/java/com/rh/ems/service/FuncionarioService.java
+++ src/main/java/com/rh/ems/service/FuncionarioService.java
@@ -2,8 +2,12 @@ package com.rh.ems.service;
 
 import com.rh.ems.dto.FuncionarioDto;
 
+import java.util.List;
+
 public interface FuncionarioService {
     FuncionarioDto createFuncionario(FuncionarioDto funcionarioDto);
 
     FuncionarioDto getFuncionarioById(Long funcionarioId);
+
+    List<FuncionarioDto> getAllFuncionarios();
 }
