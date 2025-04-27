fa01f3a55d7cf2a021728ea5b36ebf6632c75dcf
João Pedro <jpedrog475@gmail.com>
2025-04-26 21:45:57 -0300

    Implementação de exceção

--- src/main/java/com/rh/ems/service/impl/FuncionarioServiceImpl.java
+++ src/main/java/com/rh/ems/service/impl/FuncionarioServiceImpl.java
@@ -2,6 +2,7 @@ package com.rh.ems.service.impl;
 
 import com.rh.ems.dto.FuncionarioDto;
 import com.rh.ems.entity.Funcionario;
+import com.rh.ems.exception.NotFoundException;
 import com.rh.ems.mapper.FuncionarioMapper;
 import com.rh.ems.repository.FuncionarioRepository;
 import com.rh.ems.service.FuncionarioService;
@@ -19,5 +20,13 @@ public class FuncionarioServiceImpl implements FuncionarioService {
         Funcionario funcionario = FuncionarioMapper.mapToFuncionario(funcionarioDto);
         Funcionario savedFuncionario = funcionarioRepository.save(funcionario);
         return FuncionarioMapper.mapToFuncionarioDto(savedFuncionario);
+    }
+
+    @Override
+    public FuncionarioDto getFuncionarioById(Long funcionarioId) {
+       Funcionario funcionario = funcionarioRepository.findById(funcionarioId)
+                .orElseThrow(()
+                        -> new NotFoundException("Funcionário não foi encontrado com este Id : " + funcionarioId));
+        return FuncionarioMapper.mapToFuncionarioDto(funcionario);
     }
 }
